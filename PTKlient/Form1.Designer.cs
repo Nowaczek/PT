54ffed0ee5b86908b285d1fe72f5043ac0e0ee83
Nowaczek <nowaczek007@wp.pl>
2015-05-05 20:12:42 +0200

    0.001

---
+++ PTKlient/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PTKlient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPołącz = new System.Windows.Forms.Button();
+            this.comboBoxLocalIP = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxserverIP = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // buttonPołącz
+            // 
+            this.buttonPołącz.Location = new System.Drawing.Point(97, 218);
+            this.buttonPołącz.Name = "buttonPołącz";
+            this.buttonPołącz.Size = new System.Drawing.Size(75, 23);
+            this.buttonPołącz.TabIndex = 0;
+            this.buttonPołącz.Text = "Połącz";
+            this.buttonPołącz.UseVisualStyleBackColor = true;
+            this.buttonPołącz.Click += new System.EventHandler(this.buttonPołącz_Click);
+            // 
+            // comboBoxLocalIP
+            // 
+            this.comboBoxLocalIP.FormattingEnabled = true;
+            this.comboBoxLocalIP.Location = new System.Drawing.Point(133, 26);
+            this.comboBoxLocalIP.Name = "comboBoxLocalIP";
+            this.comboBoxLocalIP.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLocalIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LocalIP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "serverlIP";
+            // 
+            // textBoxserverIP
+            // 
+            this.textBoxserverIP.Location = new System.Drawing.Point(142, 70);
+            this.textBoxserverIP.Name = "textBoxserverIP";
+            this.textBoxserverIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxserverIP.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBoxserverIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxLocalIP);
+            this.Controls.Add(this.buttonPołącz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPołącz;
+        private System.Windows.Forms.ComboBox comboBoxLocalIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxserverIP;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ PTKlient/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTKlient
+{
+    public partial class Form1 : Form
+    {
+        string localIP;
+        IPAddress serverIP;
+        int serverPort;
+        int serwerKomendPort = 1978;
+        public Form1()
+        {
+            InitializeComponent();
+            IPHostEntry adresIP = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var s in adresIP.AddressList)
+            {
+                string[] parts = s.ToString().Split('.');
+                if (parts.Length == 4)
+                {
+                    comboBoxLocalIP.Items.Add(s.ToString());
+                }
+            }
+        }
+
+        private void buttonPołącz_Click(object sender, EventArgs e)
+        {
+            localIP = comboBoxLocalIP.SelectedItem.ToString();
+            serverIP = IPAddress.Parse(textBoxserverIP.Text);
+            serverPort = 10000;
+
+            WyslijWiadomoscUDP(localIP + ":LOGIN:" + "Piotr:" + "Nowak:" + "106");//+ tb_imie.Text + ":" + tb_nazwisko.Text + ":" + tb_index.Text);
+
+            //notifyIcon1.Visible = true;
+            //notifyIcon1.Text = "Program Minimalizujący";
+            //notifyIcon1.Icon = this.Icon;
+            //notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            //this.ShowInTaskbar = false;
+            //this.Visible = false;
+
+            //timer1.Start(); // wysyla strony www
+            backgroundWorker1.RunWorkerAsync(); // oczekuje na komunikaty
+        }
+
+
+
+        private void WyslijWiadomoscUDP(string wiadomosc)
+        {
+            UdpClient klient = new UdpClient(serverIP.ToString(), 43210);
+            byte[] bufor = Encoding.ASCII.GetBytes(wiadomosc);
+            klient.Send(bufor, bufor.Length);
+            klient.Close();
+        }
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            TcpListener serwer = new TcpListener(IPAddress.Parse(localIP), serwerKomendPort);
+            serwer.Start();
+            //this.SetText("Oczekuje na komendy...");
+            while (true)
+            {
+                try
+                {
+                    TcpClient klientKomend = serwer.AcceptTcpClient();
+                    //this.SetText("Otrzymano komende.");
+                    NetworkStream ns = klientKomend.GetStream();
+                    Byte[] bufor = new Byte[100];
+                    int odczyt = ns.Read(bufor, 0, bufor.Length);
+                    String s = Encoding.ASCII.GetString(bufor);
+                    string wiadomosc = Encoding.ASCII.GetString(bufor);
+                    /*
+                    if (wiadomosc.Contains("##S##"))
+                    {
+                        //this.SetText("Zrzut ekranu w trakcie wykonywania ...");
+                        obraz = wykonajScreenshot();
+                        MemoryStream ms = new MemoryStream();
+                        obraz.Save(ms, ImageFormat.Jpeg);
+                        byte[] obrazByte = ms.GetBuffer();
+                        ms.Close();
+                        try
+                        {
+                            TcpClient klient2 = new TcpClient(serwerDanychIP.ToString(), serwerDanychPort);//
+                            NetworkStream ns2 = klient2.GetStream();
+                            //this.SetText("Wysyłanie zrzutu ...");
+                            using (BinaryWriter bw = new BinaryWriter(ns2))
+                            {
+                                bw.Write((int)obrazByte.Length);
+                                bw.Write(obrazByte);
+                            }
+                            //this.SetText("Zrzut ekranu przesłany");
+                        }
+
+                    }*/
+                    
+                }
+                catch { }
+            }
+        }
+    }
+}
