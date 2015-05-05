ea81563d8b3dc0ce3e1de406d3d447278c25faf7
Nowaczek <nowaczek007@wp.pl>
2015-05-06 00:29:44 +0200

    Poprawki klienta

--- PTKlient/Form1.cs
+++ PTKlient/Form1.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Activity_Monitor_Client;
 
@@ -43,22 +39,14 @@ namespace PTKlient
             serverIP = IPAddress.Parse(textBoxserverIP.Text);
             serverPort = 10000;
 
-            WyslijWiadomoscUDP(localIP + ":LOGIN:" + "Nowaczek:" + "Nowaczek:" + "106");//+ tb_imie.Text + ":" + tb_nazwisko.Text + ":" + tb_index.Text);
-
-            //notifyIcon1.Visible = true;
-            //notifyIcon1.Text = "Program Minimalizujący";
-            //notifyIcon1.Icon = this.Icon;
-            //notifyIcon1.ContextMenuStrip = contextMenuStrip1;
-            //this.ShowInTaskbar = false;
-            //this.Visible = false;
-
-            //timer1.Start(); // wysyla strony www
-            backgroundWorker1.RunWorkerAsync(); // oczekuje na komunikaty
+            sendUDP(localIP + ":LOGIN:" + "Nowaczek:" + "Nowaczek:" + "106");
+           
+            backgroundWorker1.RunWorkerAsync();
         }
 
 
 
-        private void WyslijWiadomoscUDP(string wiadomosc)
+        private void sendUDP(string wiadomosc)
         {
             UdpClient klient = new UdpClient(serverIP.ToString(), 43210);
             byte[] bufor = Encoding.ASCII.GetBytes(wiadomosc);
@@ -72,43 +60,38 @@ namespace PTKlient
         {
             TcpListener serwer = new TcpListener(IPAddress.Parse(localIP), serwerKomendPort);
             serwer.Start();
-            //this.SetText("Oczekuje na komendy...");
             while (true)
             {
                 try
                 {
                     TcpClient klientKomend = serwer.AcceptTcpClient();
-                    //this.SetText("Otrzymano komende.");
                     NetworkStream ns = klientKomend.GetStream();
                     Byte[] bufor = new Byte[100];
                     int odczyt = ns.Read(bufor, 0, bufor.Length);
                     String s = Encoding.ASCII.GetString(bufor);
-                    string wiadomosc = Encoding.ASCII.GetString(bufor);
+                    string message = Encoding.ASCII.GetString(bufor);
                     
-                    if (wiadomosc.Contains("##S##"))
+                    if (message.Contains("##S##"))
                     {
-                        //this.SetText("Zrzut ekranu w trakcie wykonywania ...");
-                        obraz = wykonajScreenshot();
+                        obraz = zrobScreena();
                         MemoryStream ms = new MemoryStream();
                         obraz.Save(ms, ImageFormat.Jpeg);
                         byte[] obrazByte = ms.GetBuffer();
                         ms.Close();
                         try
                         {
-                            TcpClient klient2 = new TcpClient(serverIP.ToString(), serverPort);//
+                            TcpClient klient2 = new TcpClient(serverIP.ToString(), serverPort);
                             NetworkStream ns2 = klient2.GetStream();
-                            //this.SetText("Wysyłanie zrzutu ...");
                             using (BinaryWriter bw = new BinaryWriter(ns2))
                             {
                                 bw.Write((int)obrazByte.Length);
                                 bw.Write(obrazByte);
                             }
-                            //this.SetText("Zrzut ekranu przesłany");
                         }
                         catch { }
                     }
 
-                    if (wiadomosc.Contains("##P##"))
+                    if (message.Contains("##P##"))
                     {
                         string[] proc = Processes.getProcesses();
                         string wiadomoscudp = String.Empty;
@@ -116,17 +99,7 @@ namespace PTKlient
                         {
                             wiadomoscudp += p + ";";
                         }
-                        WyslijWiadomoscUDP(localIP + ":PROC:" + wiadomoscudp);
-                    }
-                    if (wiadomosc.Contains("##PB##"))
-                    {
-                        string name = wiadomosc + "#";
-                        name = wyciagnij(name, "##PB##", "##END##");
-
-                        foreach (var process in Process.GetProcessesByName(name))
-                        {
-                            process.Kill();
-                        }
+                        sendUDP(localIP + ":PROC:" + wiadomoscudp);
                     }
                     
                 }
@@ -134,17 +107,17 @@ namespace PTKlient
             }
         }
 
-        private Bitmap wykonajScreenshot()
+        private Bitmap zrobScreena()
         {
-            Bitmap bitmapa = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-            Graphics screenshot = Graphics.FromImage(bitmapa);
+            Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics screenshot = Graphics.FromImage(screen);
             screenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
-            bitmapa = zmien(bitmapa, new Size(800, 450));
-            return bitmapa;
+            screen = zmienRozmiar(screen, new Size(800, 450));
+            return screen;
         }
 
 
-        public Bitmap zmien(Bitmap imgToResize, Size size)
+        public Bitmap zmienRozmiar(Bitmap imgToResize, Size size)
         {
             Bitmap bitmap = new Bitmap(size.Width, size.Height);
             using (Graphics graphics = Graphics.FromImage((Image)bitmap))
@@ -153,29 +126,6 @@ namespace PTKlient
                 graphics.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
             }
             return bitmap;
-        }
-
-
-
-        public string wyciagnij(string source, string poczatek, string koniec)
-        {
-            try
-            {
-                string x;
-
-                int indexpoczatek = (source.IndexOf(poczatek)) + poczatek.Length;
-                x = source.Substring(indexpoczatek, source.Length - indexpoczatek);
-
-                int indexkoniec = x.IndexOf(koniec);
-
-                x = x.Substring(0, indexkoniec);
-
-                return x;
-            }
-            catch
-            {
-                return "0";
-            }
-        }
+        } 
     }
 }
