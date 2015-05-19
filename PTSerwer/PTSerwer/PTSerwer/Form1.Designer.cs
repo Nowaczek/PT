7d79d060f2f4b969f6d979acf0eced11795c5e10
michalpopowczak <michal.popowczak@student.put.poznan.pl>
2015-05-19 17:56:51 +0200

    Serwer 0.6

---
+++ PTSerwer/PTSerwer/PTSerwer/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace PTSerwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Serwer_on = new System.Windows.Forms.Button();
+            this.l_stan = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.odbierzobraz = new System.ComponentModel.BackgroundWorker();
+            this.timer_print = new System.Windows.Forms.Timer(this.components);
+            this.nasluchiwanie = new System.ComponentModel.BackgroundWorker();
+            this.timer_proces = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(805, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 21);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(12, 13);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(787, 405);
+            this.tabControl1.TabIndex = 22;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(779, 379);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Podgląd użytkownika";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(773, 338);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "podgląd wyłączony";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Włącz/Wyłącz Podgląd";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Stream_ON_OFF_Click);
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.button10);
+            this.tabPage5.Controls.Add(this.listBox2);
+            this.tabPage5.Controls.Add(this.button5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(779, 379);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Przegląd uruchomionych programów";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(156, 6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(148, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "clear";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.wyczyszc_liste_procesow);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(3, 35);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(774, 329);
+            this.listBox2.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 6);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "procesy";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.pobierz_procesy_Click);
+            // 
+            // Serwer_on
+            // 
+            this.Serwer_on.Location = new System.Drawing.Point(805, 37);
+            this.Serwer_on.Name = "Serwer_on";
+            this.Serwer_on.Size = new System.Drawing.Size(208, 28);
+            this.Serwer_on.TabIndex = 23;
+            this.Serwer_on.Text = "Włącz serwer";
+            this.Serwer_on.UseVisualStyleBackColor = true;
+            this.Serwer_on.Click += new System.EventHandler(this.Serwer_on_Click);
+            // 
+            // l_stan
+            // 
+            this.l_stan.AutoSize = true;
+            this.l_stan.Location = new System.Drawing.Point(859, 68);
+            this.l_stan.Name = "l_stan";
+            this.l_stan.Size = new System.Drawing.Size(94, 13);
+            this.l_stan.TabIndex = 24;
+            this.l_stan.Text = "Serwer wyłączony";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(805, 88);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(208, 329);
+            this.listBox1.TabIndex = 25;
+            // 
+            // odbierzobraz
+            // 
+            this.odbierzobraz.DoWork += new System.ComponentModel.DoWorkEventHandler(this.odbierzobraz_DoWork);
+            // 
+            // timer_print
+            // 
+            this.timer_print.Interval = 1500;
+            this.timer_print.Tick += new System.EventHandler(this.timer_print_Tick);
+            // 
+            // nasluchiwanie
+            // 
+            this.nasluchiwanie.DoWork += new System.ComponentModel.DoWorkEventHandler(this.nasluchiwanie_DoWork);
+            // 
+            // timer_proces
+            // 
+            this.timer_proces.Interval = 1000;
+            this.timer_proces.Tick += new System.EventHandler(this.timer_proces_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 430);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.l_stan);
+            this.Controls.Add(this.Serwer_on);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button Serwer_on;
+        private System.Windows.Forms.Label l_stan;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker odbierzobraz;
+        private System.Windows.Forms.Timer timer_print;
+        private System.ComponentModel.BackgroundWorker nasluchiwanie;
+        private System.Windows.Forms.Timer timer_proces;
+    }
+}
+
---
+++ PTSerwer/PTSerwer/PTSerwer/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace PTSerwer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            zainicjuj();
+            
+        }
+
+
+
+         List<user> listuser = new List<user>();
+         List<string> listaStringow = new List<string>();
+         public string ip;
+         private string adresIP = "none";
+         private bool podglad = false;
+         
+       
+        delegate void SetTextCallBack(string tekst);
+        private void SetText(string tekst)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetText);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                this.listBox1.Items.Add(tekst);
+            }
+        }
+
+        delegate void RemoveTextCallBack(int pozycja);
+        private void RemoveText(int pozycja)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                RemoveTextCallBack f = new RemoveTextCallBack(RemoveText);
+                this.Invoke(f, new object[] { pozycja });
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(pozycja);
+            }
+        }
+
+
+        private void zainicjuj()
+        {
+             List<user> listuser = new List<user>();
+            button1.Enabled = false;
+            button5.Enabled = false;
+            button10.Enabled = false;
+            IPHostEntry adresIP = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var s in adresIP.AddressList)
+            {
+                string[] parts = s.ToString().Split('.');
+                if (parts.Length == 4)
+                {
+                    comboBox1.Items.Add(s.ToString());
+                }
+            }
+            comboBox1.Items.Add("127.0.0.1");
+        }
+
+         private void takeaddress()
+        {
+            try
+            {
+                foreach (user u in listuser)
+                {
+                    if (u.imienazwisko == listBox1.Items[listBox1.SelectedIndex].ToString())
+                        adresIP = u.adres;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("take addres error");
+            }
+
+
+        }
+
+        private void print()
+        {
+            takeaddress();
+
+
+
+
+            if (adresIP == "none")
+            {
+                MessageBox.Show("Nie wybrałeś użytkownika");
+                timer_print.Stop();
+            }
+            else
+            {
+
+                label1.Text = "podgląd włączony";
+                
+
+
+                try
+                {
+
+
+                    TcpClient klient = new TcpClient(adresIP, 1978);
+                    NetworkStream ns = klient.GetStream();
+                    byte[] bufor = new byte[5];
+                    bufor = Encoding.ASCII.GetBytes("##S##");
+                    ns.Write(bufor, 0, bufor.Length);
+                    if (odbierzobraz.IsBusy == false)
+                        odbierzobraz.RunWorkerAsync();
+                    //czysz adres
+                    adresIP = "none";
+                    /*else
+                           MessageBox.Show("Nie mozna teraz zrealizowac zrzutu ekranu");*/
+                }
+                catch
+                {
+                    MessageBox.Show("print error");
+                }
+            }
+        }
+
+        private void proces()
+        {
+
+            takeaddress();
+
+            if (adresIP == "none")
+            {
+                MessageBox.Show("Nie wybrałeś użytkownika");
+            }
+            else
+            {
+
+
+                try
+                {
+
+                    TcpClient klient = new TcpClient(adresIP, 1978);
+                    NetworkStream ns = klient.GetStream();
+                    byte[] bufor = new byte[5];
+                    bufor = Encoding.ASCII.GetBytes("##P##");
+                    ns.Write(bufor, 0, bufor.Length);
+                    timer_proces.Start();
+                }
+                catch
+                {
+                    // MessageBox.Show("Nie zaznaczyłeś użytkownika");
+                }
+            }
+        }
+
+
+
+        private void odbierzobraz_DoWork(object sender, DoWorkEventArgs e)
+        {
+            TcpListener serwer2 = new TcpListener(IPAddress.Parse(ip), 10000);
+            //tutaj port 10000
+
+
+            serwer2.Start();
+            TcpClient klient2 = serwer2.AcceptTcpClient();
+            NetworkStream ns = klient2.GetStream();
+            byte[] obrazByte;
+            using (BinaryReader odczytObrazu = new BinaryReader(ns))
+            {
+                int rozmiarObrazu = odczytObrazu.ReadInt32();
+                obrazByte = odczytObrazu.ReadBytes(rozmiarObrazu);
+            }
+            using (MemoryStream ms = new MemoryStream(obrazByte))
+            {
+                Image obraz = Image.FromStream(ms);
+                pictureBox1.Image = obraz;
+            }
+            serwer2.Stop();
+        }
+
+        private void nasluchiwanie_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IPEndPoint zdalnyIP = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient klient = new UdpClient(43210);
+            while (true)
+            {
+                Byte[] bufor = klient.Receive(ref zdalnyIP);
+                string dane = Encoding.ASCII.GetString(bufor);
+                string[] cmd = dane.Split(new char[] { ':' });
+
+                //MessageBox.Show(cmd[1]);
+
+                if (cmd[1] == "BYE")
+                {
+                    for (int i = 0; i < listBox1.Items.Count; i++)
+                        if (listBox1.Items[i].ToString() == cmd[0])
+                            this.RemoveText(i);
+
+                    foreach (user u in listuser)
+                    {
+                        if (u.adres == cmd[0])
+                        {
+                            for (int i = 0; i < listBox1.Items.Count; i++)
+                                if (listBox1.Items[i].ToString() == u.imienazwisko)
+                                    this.RemoveText(i);
+                        }
+                    }
+
+                }
+                if (cmd[1] == "LOGIN")
+                {
+                    bool znalazl = false;
+
+                    foreach (user u in listuser)
+                    {
+                        if (u.index == cmd[4])
+                        {
+                            znalazl = true;
+                        }
+                    }
+
+                    if (znalazl == false)
+                    {
+                        user nowy = new user(cmd[2], cmd[3], cmd[4], cmd[0]);
+                        listuser.Add(nowy);
+                        this.SetText(cmd[2] + " " + cmd[3]);
+                    }
+                    else
+                    {
+                        znalazl = false;
+                    }
+                }
+
+
+
+                if (cmd[1] == "PROC")
+                {
+                   string[] tablica = cmd[2].Split(';');
+
+                    foreach (string proces in tablica)
+                    {
+                        listaStringow.Add(proces);
+                    }
+                }
+
+
+
+
+            }
+        }
+
+        private void Serwer_on_Click(object sender, EventArgs e)
+        {
+            
+                try
+                {
+                    ip = comboBox1.SelectedItem.ToString();
+                    nasluchiwanie.RunWorkerAsync();
+
+                    l_stan.Text = "Serwer włączony";
+                    button1.Enabled = true;
+                    button5.Enabled = true;
+                    button10.Enabled = true;
+                    Serwer_on.Enabled = false;
+
+                }
+                catch
+                {
+                    System.Windows.Forms.MessageBox.Show("Wybierz adres IP serwera");
+                }
+            
+           
+
+            
+        }
+
+        private void timer_print_Tick(object sender, EventArgs e)
+        {
+            print();
+        }
+
+        private void timer_proces_Tick(object sender, EventArgs e)
+        {
+            if (listaStringow.Count > 0)
+            {
+                listBox2.Items.Clear();
+                foreach (string proces in listaStringow)
+                {
+                    listBox2.Items.Add(proces);
+                }
+                listaStringow.Clear();
+                timer_proces.Stop();
+            }
+        }
+
+        private void Stream_ON_OFF_Click(object sender, EventArgs e)
+        {
+            takeaddress();
+
+
+            if (podglad == false)
+            {
+
+                if (adresIP == "none")
+                {
+                    MessageBox.Show("Nie wybrałeś użytkownika");
+                }
+                else
+                {
+                    timer_print.Start();
+                    label1.Text = "podgląd włączony";
+                    adresIP = "none";
+                    podglad = true;
+                }
+
+            }
+            else
+            {
+                timer_print.Stop();
+                label1.Text = "podgląd wyłączony";
+                adresIP = "none";
+                podglad = false;
+                pictureBox1.Image = null;
+
+            }
+        }
+
+        private void pobierz_procesy_Click(object sender, EventArgs e)
+        {
+            proces();
+        }
+
+        private void wyczyszc_liste_procesow(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
